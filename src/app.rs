4879c4d779eeff204bf558367888e88202a3b273
pesukone <j.v.aalto@gmail.com>
2025-02-17 16:18:10 +0200

    configure leptos_i18n

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,4 @@
+use crate::i18n::*;
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
@@ -13,7 +14,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
               <meta charset="utf-8"/>
               <meta name="viewport" content="width=device-width, initial-scale=1"/>
               <AutoReload options=options.clone() />
-              <HydrationScripts options/>
+              <HydrationScripts options islands=true/>
               <MetaTags/>
           </head>
           <body>
@@ -29,33 +30,47 @@ pub fn App() -> impl IntoView {
   provide_meta_context();
 
   view! {
-      // injects a stylesheet into the document <head>
-      // id=leptos means cargo-leptos will hot-reload this stylesheet
-      <Stylesheet id="leptos" href="/pkg/cv.css"/>
+    // injects a stylesheet into the document <head>
+    // id=leptos means cargo-leptos will hot-reload this stylesheet
+    <Stylesheet id="leptos" href="/pkg/cv.css"/>
 
-      // sets the document title
-      <Title text="Welcome to Leptos"/>
+    // sets the document title
+    <Title text="CV"/>
 
-      // content for this welcome page
+    // content for this welcome page
+    <I18nContextProvider>
       <Router>
-          <main>
-              <Routes fallback=|| "Page not found.".into_view()>
-                  <Route path=StaticSegment("") view=HomePage/>
-              </Routes>
-          </main>
+        <main>
+          <Routes fallback=|| "Page not found.".into_view()>
+            <Route path=StaticSegment("") view=CV/>
+          </Routes>
+        </main>
       </Router>
+    </I18nContextProvider>
   }
 }
 
-/// Renders the home page of your application.
 #[component]
-fn HomePage() -> impl IntoView {
-  // Creates a reactive value to update the button
-  let count = RwSignal::new(0);
-  let on_click = move |_| *count.write() += 1;
+fn CV() -> impl IntoView {
+  view! {
+    <h1>"CV"</h1>
+    <SwitchLang/>
+  }
+}
+
+#[island]
+fn SwitchLang() -> impl IntoView {
+  let i18n = use_i18n();
+
+  let on_switch = move |_| {
+    let new_lang = match i18n.get_locale() {
+      Locale::fi => Locale::en,
+      Locale::en => Locale::fi,
+    };
+    i18n.set_locale(new_lang);
+  };
 
   view! {
-      <h1>"Welcome to Leptos!"</h1>
-      <button on:click=on_click>"Click Me: " {count}</button>
+    <button on:click=on_switch>{t!(i18n, test_msg)}</button>
   }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod app;
 
+leptos_i18n::load_locales!();
+
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
 pub fn hydrate() {
-    use crate::app::*;
-    console_error_panic_hook::set_once();
-    leptos::mount::hydrate_body(App);
+  use crate::app::*;
+  console_error_panic_hook::set_once();
+  leptos::mount::hydrate_islands();
 }
