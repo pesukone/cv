6fc6ad71c54ebae560ef24ea68bf1ad4e9877523
pesukone <j.v.aalto@gmail.com>
2025-03-06 20:32:00 +0200

    working tailwind and translations

--- flake.nix
+++ flake.nix
@@ -5,37 +5,68 @@
     nixpkgs.url = "github:nixos/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    cargo-leptos-src = {
+      url = "github:leptos-rs/cargo-leptos";
+      flake = false;
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    rust-overlay
-  }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { 
-	  inherit system overlays;
-	};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      rust-overlay,
+      cargo-leptos-src,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [
+          (import rust-overlay)
+          (final: prev: {
+            cargo-leptos = prev.rustPlatform.buildRustPackage {
+              pname = "cargo-leptos";
+              version = "0.2.29";
 
-      in rec {
+              src = cargo-leptos-src;
+              useFetchCargoVendor = true;
+              cargoHash = "sha256-KlB4/1cqFF59xzSqQBzqWRiPoClw/uSk4Y9ZJYbm8/M=";
+
+              buildInputs = with prev.pkgs; [
+                openssl.dev
+              ];
+              nativeBuildInputs = with prev.pkgs; [
+                pkg-config
+              ];
+
+              buildFeatures = [ "no_downloads" ];
+              doCheck = false;
+            };
+          })
+        ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+      in
+      rec {
         flakedPkgs = pkgs;
 
-	devShell = pkgs.mkShell {
-	  buildInputs = with pkgs; [
-	    (rust-bin.selectLatestNightlyWith (toolchain:
-	      toolchain.default.override {
-	        extensions = [ "rust-src" ];
-		targets = [ "wasm32-unknown-unknown" ];
-	      }
-	    ))
-	    cargo-leptos
-	    sass
-	    lld
-	  ];
-	};
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.rust-bin.selectLatestNightlyWith (
+              toolchain:
+              toolchain.default.override {
+                extensions = [ "rust-src" ];
+                targets = [ "wasm32-unknown-unknown" ];
+              }
+            ))
+            cargo-leptos
+            tailwindcss_4
+            sass
+          ];
+        };
       }
     );
 }
--- src/app.rs
+++ src/app.rs
@@ -1,24 +1,27 @@
 use crate::i18n::*;
+use leptos::logging::log;
 use leptos::prelude::*;
 use leptos_meta::{provide_meta_context, MetaTags, Stylesheet, Title};
 use leptos_router::{
-  components::{Route, Router, Routes},
+  components::{Route, Router, Routes, A},
+  hooks::{use_navigate, use_query},
+  params::Params,
   StaticSegment,
 };
 
 pub fn shell(options: LeptosOptions) -> impl IntoView {
   view! {
-      <!DOCTYPE html>
+      <!DOCTYPE html> 
       <html lang="en">
           <head>
-              <meta charset="utf-8"/>
-              <meta name="viewport" content="width=device-width, initial-scale=1"/>
+              <meta charset="utf-8" />
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
               <AutoReload options=options.clone() />
-              <HydrationScripts options islands=true/>
-              <MetaTags/>
+              <HydrationScripts options islands=true />
+              <MetaTags />
           </head>
           <body>
-              <App/>
+              <App />
           </body>
       </html>
   }
@@ -30,47 +33,108 @@ pub fn App() -> impl IntoView {
   provide_meta_context();
 
   view! {
-    // injects a stylesheet into the document <head>
-    // id=leptos means cargo-leptos will hot-reload this stylesheet
-    <Stylesheet id="leptos" href="/pkg/cv.css"/>
+      <Title text="CV" />
 
-    // sets the document title
-    <Title text="CV"/>
+      <Stylesheet id="leptos" href="/pkg/cv.css" />
 
-    // content for this welcome page
-    <I18nContextProvider>
-      <Router>
-        <main>
-          <Routes fallback=|| "Page not found.".into_view()>
-            <Route path=StaticSegment("") view=CV/>
-          </Routes>
-        </main>
-      </Router>
-    </I18nContextProvider>
+      // content for this welcome page
+      <I18nContextProvider>
+          <Router>
+              <main>
+                  <Routes fallback=|| "Page not found.".into_view()>
+                      <Route path=StaticSegment("") view=CV />
+                  </Routes>
+              </main>
+          </Router>
+      </I18nContextProvider>
   }
 }
 
 #[component]
 fn CV() -> impl IntoView {
   view! {
-    <h1>"CV"</h1>
-    <SwitchLang/>
+      <div class="flex flex-col text-center">
+          <h1>"CV"</h1>
+          <SwitchLang />
+          <ParamTest />
+          <Skills />
+      </div>
   }
 }
 
-#[island]
+#[derive(Params, PartialEq)]
+struct QueryParams {
+  lang: Option<String>,
+}
+
+#[component]
+fn ParamTest() -> impl IntoView {
+  let params = use_query::<QueryParams>();
+  let lang = params
+    .read()
+    .as_ref()
+    .ok()
+    .and_then(|params| params.lang.clone())
+    .unwrap_or("fi".to_string());
+
+  view! { <div>{lang}</div> }
+}
+
+#[component]
 fn SwitchLang() -> impl IntoView {
+  let params = use_query::<QueryParams>();
   let i18n = use_i18n();
 
-  let on_switch = move |_| {
-    let new_lang = match i18n.get_locale() {
-      Locale::fi => Locale::en,
-      Locale::en => Locale::fi,
-    };
-    i18n.set_locale(new_lang);
+  let lang = params
+    .read()
+    .as_ref()
+    .ok()
+    .and_then(|params| params.lang.clone())
+    .unwrap_or_default();
+
+  match lang.as_str() {
+    "en" => i18n.set_locale(Locale::en),
+    "fi" => i18n.set_locale(Locale::fi),
+    _ => i18n.set_locale(Locale::fi),
+  }
+
+  let lang = match i18n.get_locale() {
+    Locale::fi => "en",
+    Locale::en => "fi",
   };
 
   view! {
-    <button on:click=on_switch>{t!(i18n, test_msg)}</button>
+      <A
+          href=format!("/?lang={}", lang)
+          attr:class="text-medium text-blue-600 dark:text-blue-500 hover:underline"
+      >
+          {t!(i18n, test_msg)}
+      </A>
   }
+}
+
+#[component]
+fn Skills() -> impl IntoView {
+  let skills = vec![
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "CSS",
+    "React",
+    "Redux",
+    "REST",
+    "GraphQL",
+    "Node.js",
+    "SQL",
+    "Docker",
+    "Kubernetes",
+    "Nix",
+    "Ansible",
+    "C++",
+    "Go",
+    "Rust",
+  ]
+  .join(" ");
+
+  view! { <div class="flex">{skills}</div> }
 }
